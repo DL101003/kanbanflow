4883fbb374b194a52e70d595c69d4d97d7556f42
Hoang Van Loc <hani101003@gmail.com>
2025-11-07 21:07:21 +0700

    First Commit

---
+++ kanbanflow/src/test/java/com/project/kanbanflow/KanbanflowApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.kanbanflow;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KanbanflowApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
